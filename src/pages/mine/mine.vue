081967189a63a2602b2c477f39e3a3ad44254ce7
touero <touer0018@gmail.com>
2024-10-17 16:38:38 +0800

    feat: try to add wx login action

--- src/pages/mine/mine.vue
+++ src/pages/mine/mine.vue
@@ -1,13 +1,49 @@
 <template>
   <view class="user-image-container">
-    <image class="user-image" src="/static/mine.png" mode="aspectFill" />
-    <button>Login</button>
+    <image class="user-image" :src="userAvatar" mode="aspectFill" />
+    <button @click="loginWithWeChat">Login</button>
   </view>
 </template>
 
+
 <script setup lang="ts">
 import { ref } from 'vue'
+
+// 设置自己的头像 URL
+const defaultAvatar = '/static/mine.png' // 替换为你自己的头像路径
+const userAvatar = ref(defaultAvatar) // 初始值设置为自己的头像
+
+// 微信登录并获取头像和昵称
+const loginWithWeChat = () => {
+  // 调用微信登录
+  wx.login({
+    success(res) {
+      if (res.code) {
+        // 使用 wx.getUserProfile 获取用户的头像和昵称
+        wx.getUserProfile({
+          desc: '用于完善用户资料', // 必填，获取用户信息时的提示
+          success(profileRes) {
+            const { avatarUrl, nickName } = profileRes.userInfo
+            userAvatar.value = avatarUrl // 设置用户头像
+
+            // 提示用户登录成功
+            uni.showToast({ title: `欢迎, ${nickName}`, icon: 'none' })
+          },
+          fail() {
+            uni.showToast({ title: '获取用户信息失败', icon: 'none' })
+          }
+        })
+      } else {
+        uni.showToast({ title: '微信登录失败', icon: 'none' })
+      }
+    },
+    fail() {
+      uni.showToast({ title: '微信登录接口调用失败', icon: 'none' })
+    }
+  })
+}
 </script>
+
 
 <style>
 .user-image-container {
